5b417668abd608f9fc2cafab10594a84d10da4ba
Aurelien Carmes <carmesaure@cy-tech.fr>
2022-12-16 20:55:14 +0100

    fix nom fichier main

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 
 from app.api.api import api_router
+from app.train import TrainClass
 
 
 
 app = FastAPI(title="Wine Quality Prediction")
 
+wineModel = TrainClass('base')
+print('Model loaded')
 
 
 app.include_router(api_router, prefix="/api")
+
+
+
+# @app.on_event("startup")
+# def init_model():
+#     global wineModel
